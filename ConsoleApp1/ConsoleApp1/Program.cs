2882cef8c94f8fa663bc5891e890714d636cfadf
Olha Hlazkova <maradudikk@gmail.com>
2017-11-29 23:37:39 +0200

    addition of project files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BinaryTreeLibrary;
+
+namespace Hlazkova_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* 
+            var tree = new BinaryTree();
+            tree.Insert(20);
+            tree.Insert(40);
+            tree.Insert(10);
+            tree.Insert(30);
+            tree.Insert(80);
+            tree.Insert(29);
+            tree.Insert(31);
+            tree.Insert(32);
+            tree.Insert(70);
+            BinaryTreeExtensions.Print(tree);
+            tree.Remove(40);
+            BinaryTreeExtensions.Print(tree);
+            tree.Remove(20);
+            BinaryTreeExtensions.Print(tree);
+            */
+
+            /*
+                        
+                BinTree<int> integerTree = new BinTree<int>();
+
+                Random rand = new Random();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    int value = rand.Next(100);
+                    Console.WriteLine("Adding {0}", value);
+                    integerTree.Add(value);
+                }
+
+                Console.WriteLine("Number of nodes is {0}", integerTree.Count);
+                Console.WriteLine("Max value is {0}", integerTree.MaxValue);
+                Console.WriteLine("Min value is {0}", integerTree.MinValue);
+                Console.WriteLine("Preorder traversal:");
+                Console.WriteLine(string.Join(" ", integerTree.Preorder()));
+                Console.WriteLine("Inorder traversal:");
+                Console.WriteLine(string.Join(" ", integerTree.Inorder()));
+                Console.WriteLine("Postorder traversal:");
+                Console.WriteLine(string.Join(" ", integerTree.Postorder()));
+                Console.WriteLine("Levelorder traversal:");
+                Console.WriteLine(string.Join(" ", integerTree.Levelorder()));
+                Console.WriteLine("Default traversal (inorder):");
+                foreach (int n in integerTree)
+                    Console.Write("{0} ", n);
+                Console.WriteLine();
+                Console.ReadKey(true);
+            */
+
+            BinTree<Student> s = new BinTree<Student>();
+
+            Student Student1 = new Student("Ivanov","Maths", new DateTime(2011, 6, 10), 4);
+            Student Student2 = new Student("Ivanov", "Maths", new DateTime(2011, 6, 10), 4);
+            Student Student3 = new Student("Ivanov", "Maths", new DateTime(2011, 6, 10), 4);
+            Student Student4 = new Student("Ivanov", "Maths", new DateTime(2011, 6, 10), 4);
+            Student Student5 = new Student("Ivanov", "Maths", new DateTime(2011, 6, 10), 4);
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hlazkova_8
+{
+    //Информация о студенте может содержать поля, хранящие:
+    //имя студента, 
+    //название теста, 
+    //дату его прохождения 
+    //оценку теста для данного студента
+
+    public struct Student
+    {
+        public String Name;
+        public String Test;
+        public DateTime DateOfTest;
+        public byte Mark;
+
+        public Student(String name, String test, DateTime date, byte mark)
+        {
+            Name = name;
+            Test = test;
+            DateOfTest = date;
+            Mark = mark;
+        }
+    }
+}
